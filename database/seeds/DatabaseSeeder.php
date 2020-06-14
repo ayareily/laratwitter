03ca33e9356019e70ff686c67e9385335cfce528
ojou <musiclear05@gmail.com>
2020-06-14 14:41:16 +0900

    dbseed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            TweetsTableSeeder::class,
+            CommentsTableSeeder::class,
+            FavoriteTableSeeder::class,
+            FollowersTableSeeder::class
+            ]);
     }
 }
