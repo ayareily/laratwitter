8d583f1de8a140546f3136769157186806c864f3
ojou <musiclear05@gmail.com>
2020-06-15 16:18:06 +0900

    usercontroller edit

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -3,13 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Tweet;
+use App\Models\Comment;
+use App\Models\Follower;
 
 class TweetsConroller extends Controller
 {
     // 一覧表示
     public function index()
     {
-        //
+      $user = auth()->user();
+      $follow_ids = $follower->followingIds($user->id);
+      // followed_idだけ抜き出す
+      $following_ids = $follow_ids->pluck('followed_id')->toArray();
+
+      $timelines = $tweet->getTimelines($user->id, $following_ids);
+
+      return view('tweets.index', [
+        'user' => $user,
+        'timelines' => $timelines
+      ]);
     }
 
     // 新規ツイート入力画面
